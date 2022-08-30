878d46e8d3c5d166d0bb5dbd57d0a4fb85de4545
muzammillcoste <muzammilrizvi977@gmail.com>
2022-08-30 11:49:28 +0500

    change func name annu_salary to annul_salary

--- aggregation.py
+++ aggregation.py
@@ -2,7 +2,7 @@ class Salary:
     def __init__(self,pay,bonus):
         self.pay=pay
         self.bonus=bonus
-    def annua_salary(self):
+    def annual_salary(self):
        return self.pay*12+self.bonus
 class Employee:
     def __init__(self,name,age,salary_per):
@@ -10,7 +10,7 @@ class Employee:
         self.age=age
         self.obj_salary=salary_per
     def total_(self):
-        return self.obj_salary.annua_salary()
+        return self.obj_salary.annual_salary()
 sar1=Salary(50000,10000)
 emp1=Employee('muzammil',20,sar1)
 print(emp1.total_salary())
